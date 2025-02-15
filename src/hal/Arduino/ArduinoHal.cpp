d763760283dafe3023d97987e4ec9576b4baa33a
Crsarmv7l <85343771+Crsarmv7l@users.noreply.github.com>
2025-02-15 13:31:11 +0100

    NOP timing explanation

--- src/hal/Arduino/ArduinoHal.cpp
+++ src/hal/Arduino/ArduinoHal.cpp
@@ -107,6 +107,12 @@ void ArduinoHal::spiTransfer(uint8_t* out, size_t len, uint8_t* in) {
 }
 
 void ArduinoHal::fifoTransfer(uint8_t* out, size_t len) {
+  // ESP32-S2 2 clock cycles execution, 4 cycles to get the next instruction
+  // NOP execution time at various CPU speeds, adjust to get 100nansecond delay required for burst writes
+  // 240mhz/6 cycles = 40; 1000*(1/40) = NOP takes ~ 25 nanoseconds
+  // 160mhz/6 cycles = 26; 1000*(1/26) = NOP takes ~ 38 nanoseconds
+  // 80mhz/ 6 cycles = 13; 1000*(1/13) = NOP takes ~ 77 nanoseconds
+  // SPI can be cranked provided the appropriate delays are inserted between each byte (NOPS)
   spi->transfer(0x7F);
   for(size_t i = 0; i < len; i++) {
     spi->transfer(out[i]);
